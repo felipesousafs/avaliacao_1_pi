22b799b44b24c960979875e1d3cc2c493cc39b0b
Felipe Sousa <felipe.sousa.fs@gmail.com>
2018-10-16 17:50:39 -0300

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+  $("#header").load("shared/header.html");
+  $("#left").load("shared/left.html");
+  $("#right").load("shared/right.html");
+  $("#footer").load("shared/footer.html");
+});
